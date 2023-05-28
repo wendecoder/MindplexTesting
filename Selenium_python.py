24fab970587bd41cf4a2c02d5348d6c7e9dc0b40
wendecoder <wenonezra258@gmail.com>
2023-05-28 21:58:56 +0300

    Update Selenium_python.py

--- Selenium_python.py
+++ Selenium_python.py
@@ -13,9 +13,30 @@ driver = webdriver.Firefox(firefox_binary=firefox_binary_path, executable_path=g
 
 driver.get("https://staging.mindplex.ai/")
 
-button = driver.find_element(By.ID, "signup")
+button1 = driver.find_element(By.ID, "signup")
 time.sleep(15)
-button.click()
+button1.click()
 
-popup = driver.find_element(By.ID, "signupModal")
-assert popup.is_displayed()
+popup1 = driver.find_element(By.ID, "signupModal")
+assert popup1.is_displayed()
+time.sleep(5)
+
+closeModal1 = driver.find_element(By.ID, "close-signup")
+closeModal1.click()
+is_closed = closeModal1.is_displayed()
+assert not is_closed
+
+time.sleep(10)
+
+button2 = driver.find_element(By.ID, "signin")
+time.sleep(15)
+button2.click()
+
+popup2 = driver.find_element(By.ID, "signinModal")
+assert popup2.is_displayed()
+time.sleep(5)
+
+closeModal2 = driver.find_element(By.ID, "close-signin")
+closeModal2.click()
+is_closed = closeModal2.is_displayed()
+assert not is_closed
